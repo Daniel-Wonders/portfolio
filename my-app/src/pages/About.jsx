13834efd0a9f03bcbe030436d2f8992a264593ea
Daniel-Wonders <lautaroalloco@gmail.com>
2024-08-07 00:01:19 -0300

    More about page progress
    a lot of css

--- my-app/src/pages/About.jsx
+++ my-app/src/pages/About.jsx
@@ -9,7 +9,7 @@ export default function About(){
             <div id="AboutHero">
                 <img id="aboutPic" src={process.env.PUBLIC_URL+"/coolPicture.jpg"} />
                 <div id="aboutTxt">
-                    <h1>I'm a student, also, a developer</h1>
+                    <h1>Student, also, developer</h1>
                     <p>Since I was young, I always liked the feeling of 
                         having a computer do what I want. With this in mind I enrolled on a 
                         technical school, wich allowed me to cement the feeling that
@@ -19,24 +19,28 @@ export default function About(){
                         Learning those technologies was a lot of fun so after graduating as a CS technitian
                         I enrolled in the Computer Science career in the University of Buenos Aires (wich I'm currently in)
                     </p>
+                    <p>Ever since I was 15, I never stopped working on fun projects just for the sake of it.
+                    and some time ago, I started to learn through courses and by myself, web development 
+                    which I found really fun to play with. 
+                    </p>
                 </div>
             </div>
             <div id="skillsSection">
                 <div className="skill">
-                    <img className="icon" src={process.env.PUBLIC_URL+"https://static-00.iconduck.com/assets.00/javascript-js-icon-2048x2048-nyxvtvk0.png"}/>
-                    Javascript</div>
+                    <img className="icon" src="https://static-00.iconduck.com/assets.00/javascript-js-icon-2048x2048-nyxvtvk0.png"/>
+                    <p>Javascript</p></div>
                 <div className="skill">
-                    <img className="icon" src="https://cdn.iconscout.com/icon/premium/png-256-thumb/html-2752158-2284975.png?f=webp"/>
-                    HTML5</div>
+                    <img className="icon" src="https://cdn-icons-png.flaticon.com/256/3128/3128323.png"/>
+                    <p>HTML5</p></div>
                 <div className="skill">
-                    <img className="icon" src={process.env.PUBLIC_URL+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJDwBd9LoQzBAZigXXxzQ0kKn6TwyrE0Y7Rg&s"}/>
-                    CSS</div>
+                    <img className="icon" src="https://img.icons8.com/color/200/css3.png"/>
+                    <p>CSS</p></div>
                 <div className="skill">
-                    <img className="icon" src={process.env.PUBLIC_URL+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlGmKtrnxElpqw3AExKXPWWBulcwjlvDJa1Q&s"}/>
-                    React</div>
+                    <img className="icon" src="https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png"/>
+                    <p>React</p></div>
                 <div className="skill">
-                    <img className="icon" src={process.env.PUBLIC_URL+"https://www.svgrepo.com/show/354262/react-router.svg"}/>
-                    React Router</div>
+                    <img className="icon" src="https://www.svgrepo.com/show/354262/react-router.svg"/>
+                    <p>React Router</p></div>
 
             </div>
         </>
