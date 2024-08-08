00431279b6e61dd162670d84462c5dda5adf5957
Daniel-Wonders <lautaroalloco@gmail.com>
2024-08-07 21:39:41 -0300

    Started projects page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import {BrowserRouter,Outlet,Route,Routes} from 'react-router-dom'
 import Layout from "./pages/Layout"
 import Home from "./pages/Home"
-import About from "./pages/About.jsx"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
           <Route path="" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path='about' element={<About />} />
-            <Route path='projects' element={<>projects</>} />
+            <Route path='projects' element={<Projects />} />
             <Route path='contact' element={<>contact</>} />
           </Route>
         </Routes>
---
+++ my-app/src/pages/Projects.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default function Projects(){
+
+
+    return(
+        <div id="projectContainer">
+            <div id="project1" className="project">Project 1</div>
+            <div id="project2" className="project">Project 2</div>
+            <div id="project3" className="project">Project 3</div>
+        </div>
+    )
+}
